0d0e58f77e6e65b9cf937b65138ffb9891f7eff4
Isaac Adams <isaac.d.adams@gmail.com>
2021-11-14 17:17:25 -0500

    reading available pokemon from database file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::{io::Error};
 use rand::Rng;
 
 fn main() {
@@ -12,14 +13,47 @@ fn main() {
 
 fn search_for_wild_pokemon() {
     println!("searching for wild pokemon");
+    let dex = Pokedex::new().unwrap();
 
-    let available_pokemon = pokemon_in_wild();
+    let available_pokemon = dex.pokemon;
     let index = rand::thread_rng().gen_range(0,available_pokemon.len());
-    let encountered_pokemon = available_pokemon[index];
+    let encountered_pokemon = available_pokemon.get(index).unwrap();
 
-    println!("found a wild {}!!", encountered_pokemon);
+    println!("found a wild {} {}!!", encountered_pokemon.number, encountered_pokemon.name);
 }
 
-fn pokemon_in_wild() -> [&'static str; 3] {
-    return ["charmander", "squirtle", "caterpie"];
+struct Pokedex {
+    pokemon: Vec<Pokemon>,
+}
+
+struct Pokemon {
+    number: String,
+    name: String,
+}
+
+impl Pokedex {
+    fn new() -> Result<Pokedex, Error> {
+        let contents = std::fs::read_to_string("pokedex.db")?;
+        let mut pokemon = Vec::new();
+
+        for line in contents.lines() {
+            let chunks: Vec<&str> = line.split(' ').collect();
+            
+            if chunks.len() != 2 {
+                todo!("either the delimiter is wrong or there is corrupted data");
+            }
+
+            let number = chunks[0].to_owned();
+            let name = chunks[1].to_owned();
+
+            pokemon.push(Pokemon {
+                number,
+                name
+            });
+        }
+
+        Ok(Pokedex {
+            pokemon,
+        })
+    }
 }
