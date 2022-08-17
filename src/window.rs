ea57a8ba8ebc91794cf66ec03e1103cb8def84ed
Isaac Adams <isaac.d.adams@gmail.com>
2022-08-17 09:23:59 -0400

    fixed coloring issue

--- src/window.rs
+++ src/window.rs
@@ -5,7 +5,7 @@ use show_image::{create_window, ImageInfo, ImageView};
 pub fn display_image(window_name: &str, image: Bytes) -> Result<(), Box<dyn std::error::Error>> {
     let image = image::load_from_memory(&image)?.into_rgb8();
     let (width, height) = image.dimensions();
-    let image_view = ImageView::new(ImageInfo::bgr8(width, height), &image.as_bytes());
+    let image_view = ImageView::new(ImageInfo::rgb8(width, height), &image.as_bytes());
 
     // Create a window with default options and display the image.
     let window = create_window(window_name, Default::default())?;
